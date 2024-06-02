97921c2460cc44f54bbf708282a11d1cb9b216a7
rushad-pluto <rushad.ahmed@plutomoney.in>
2024-06-02 18:26:16 +0530

    Added Hero Section

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,7 +4,7 @@ type ButtonProps = {
   type: "button" | "submit";
   title: string;
   icon?: string;
-  variant: "btn_dark_green";
+  variant: "btn_dark_green" | "btn_green" | "btn_white_text";
 };
 
 const Button = ({ type, title, icon, variant }: ButtonProps) => {
